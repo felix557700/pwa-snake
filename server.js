89401f367768a429c92e40f75a708b01caac2d9f
Filip Vitas <felix557700@hotmail.com>
2016-12-19 23:08:21 +0100

    delete commented stuff for https server (https default from heroku)

--- server.js
+++ server.js
@@ -3,41 +3,21 @@
  */
 var express = require('express');
 var http = require('http');
-var https = require('https');
 var path = require('path');
-var fs = require('fs')
 var app = express();
 
 var static_path = path.join(__dirname, 'src');
-
-// var credentials = {
-//     key: fs.readFileSync('src/ssl/key.pem'),
-//     cert: fs.readFileSync('src/ssl/cert.pem')
-// };
-
-// function ensureSecure(req, res, next){
-//     if(req.secure){
-//         return next();
-//     }
-//     res.redirect('https://' + req.hostname + req.url);
-// }
-
-// app.all('*', ensureSecure);
 
 app.use(express.static(static_path))
     .get('/*', function (req, res) {
         res.sendFile('index.html', { root: static_path });
     })
 
-
 var httpServer = http.createServer(app);
-// var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(process.env.PORT || 8080, process.env.HOST_NAME || 'localhost', function (err) {
-    if (err) { console.log(err) }
+    if (err) {
+        console.log(err)
+    }
     console.log('Its show time!');
 });
-// httpsServer.listen(8443, function (err) {
-//     if (err) { console.log(err) }
-//     console.log('Listening at port 8443');
-// })
